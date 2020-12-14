741688c26b42bea66236d4d9b7ecf5ad22c34b28
Carlos Doria <carlosdoria953@gmail.com>
2020-12-14 15:13:33 -0300

    feat/style: Created all the logic of the game and added the rest of the styles.

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
 const cards = document.querySelectorAll('.card');
 
+let firstCard, secondCard;
+let hasFlippedCard = false;
+let lockBoard = false;
+
 function flipCard () {
-  this.classList.toggle('flip')
-}
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true
+    firstCard = this;
+    return;
+};
+
+  secondCard = this;
+  hasFlippedCard = false;
+  checkForMath();
+};
+
+function checkForMath () {
+  if (firstCard.dataset.card === secondCard.dataset.card) {
+    disableCard();
+    return;
+  }
+
+  unflipCard();
+};
+
+function disableCard () {
+  firstCard.removeEventListener(click, flipCard);
+  secondCard.removeEventListener(click, flipCard);
+
+  resetBoard()
+};
+
+function unflipCard () {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard()
+  }, 1500);
+};
+
+function resetBoard () {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+};
+
+(function shuffle () {
+  cards.forEach (card => {
+    let randomPosition = Math.floor(Math.random() * 12);
+
+    card.style.order = randomPosition;
+  })
+})();
 
 cards.forEach(card => {
   card.addEventListener('click', flipCard)
-})
+});
